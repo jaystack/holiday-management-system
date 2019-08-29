a7b26d96a16b891f1d72129b428a4ee0e71b1261
Imre Kiss <imre.kiss@jaystack.com>
2019-08-29 11:49:20 +0200

    Remove the example files from the project

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import { reducer as example } from './example';
 import { reducer as app } from './app';
 
 export default combineReducers({
-  example,
   app
 });
