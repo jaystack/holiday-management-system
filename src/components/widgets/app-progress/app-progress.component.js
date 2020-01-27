0b144f5eb032ff6c571038b82c6b62982a1fe911
sabicd <daniel.sabic@jaystack.com>
2020-01-27 16:07:29 +0100

    Remove unecessary useEffect hook, remove brackets and return keyword

--- src/components/widgets/app-progress/app-progress.component.js
+++ src/components/widgets/app-progress/app-progress.component.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,24 +7,19 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const AppProfile = ({ isAppWaiting }) => {
-  useEffect(() => {
-    console.log(isAppWaiting);
-  }, [isAppWaiting]);
-  return (
-    <Dialog
-      aria-labelledby="simple-dialog-title"
-      open={isAppWaiting}
-    >
-      <DialogActions>
-        <DialogTitle>Loading</DialogTitle>
-        <DialogContent>
-          <CircularProgress />
-        </DialogContent>
-      </DialogActions>
-    </Dialog>
-  );
-};
+const AppProfile = ({ isAppWaiting }) => (
+  <Dialog
+    aria-labelledby="simple-dialog-title"
+    open={isAppWaiting}
+  >
+    <DialogActions>
+      <DialogTitle>Loading</DialogTitle>
+      <DialogContent>
+        <CircularProgress />
+      </DialogContent>
+    </DialogActions>
+  </Dialog>
+);
 
 export default AppProfile;
 
